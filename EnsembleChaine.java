6c93a898516b6d26116cf959aa80a763ba6324b1
Ysnbeats <57399013+Ysnbeats@users.noreply.github.com>
2019-11-05 08:29:46 -0400

    Add files via upload

---
+++ EnsembleChaine.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class EnsembleChaine {
+
+	
+ArrayList< String > liste;
+public EnsembleChaine(){
+liste = new ArrayList<String>() ;
+}
+//Ajoute chaque chaine de tab, si elle n’existe pas déjà, à l’ensemble
+public boolean ajouter(String[] tab) {
+	for (int i = 0; i <n; i++) {
+		liste.add(tab[i]) 
+	 }
+	
+return false ;
+}
+//Test si l’ensemble e est équivalent à l’ensemble courant
+public boolean equals(EnsembleChaine e) {
+return false ;
+}
+//Renvoie l’union de l’ensemble e et l’ensemble courant
+public EnsembleChaine union(EnsembleChaine e){
+return null ;
+}
+//Renvoie l’intersection de l’ensemble e et l’ensemble courant
+public EnsembleChaine intersection(EnsembleChaine e){
+return null ;
+}
+//Renvoie l’union disjointe de l’ensemble e et l’ensemble courant
+public EnsembleChaine unionDisjointe(EnsembleChaine e){
+return null ;
+}
+}
